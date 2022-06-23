701e52f2bc89b80ea3ec9c6ddab0bc2359dd30e3
woonha <woon.woon.jo@gmail.com>
2022-06-23 10:31:21 +0900

    test

---
+++ src/main/java/com/bit/bubchatbot/BuBChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.bit.bubchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuBChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuBChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bit/bubchatbot/BuBChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.bubchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuBChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
